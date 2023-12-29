d0e067121e967fd48ee4abfd6d8ec6370fcfbe56
sankthomas <thomassankara74@gmail.com>
2023-12-29 21:38:40 +0300

    update: about us page

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -1,27 +1,31 @@
 import Image from "next/image";
+import React from "react";
 
-export default function About() {
+export default function AboutUs() {
   return (
-    <section className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-      <div classNamme="max-width py-20 lg:py-44 flex items-center justify-center text-center md:grid md:grid-cols-2">
-        <article>
-          <h1 className="text-white">About us</h1>
-          <p className="text-white/75">
-            Autem iste provident explicabo natus magnam temporibus quasi, maxime
-            veritatis perspiciatis libero. Esse voluptas consequuntur, non
-            beatae distinctio nostrum voluptatem sapiente ipsum!
-          </p>
-        </article>
+    <>
+      <section className="bg-gradient-to-r from-indigo-500 to-pink-500">
+        <div className="max-width py-20 lg:py-44 flex flex-col items-center justify-center gap-4 text-center space-y-8">
+          <article className="md:max-w-lg space-y-4">
+            <h1 className="text-white">About us</h1>
+            <p className="text-white/80">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
+              reprehenderit est expedita! Tempora facilis adipisci, consectetur
+              reiciendis fugit praesentium modi. In quo rem odit quod nemo,
+              deserunt dolor maxime eligendi!
+            </p>
+          </article>
 
-        <article>
-          <Image
-            src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="About us"
-            width={800}
-            height={800}
-          />
-        </article>
-      </div>
-    </section>
+          <article>
+            <Image
+              src="https://images.unsplash.com/photo-1617450365226-9bf28c04e130?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              width={800}
+              height={800}
+              alt="About us image"
+            />
+          </article>
+        </div>
+      </section>
+    </>
   );
 }
