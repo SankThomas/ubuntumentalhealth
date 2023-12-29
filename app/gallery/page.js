ee31722c6320dfece19463001e5a075afba0cb03
Thomas Sankara <thomassankara74@gmail.com>
2023-12-29 09:45:36 +0000

    chore: update gallery page

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,5 +1,17 @@
-import React from "react";
+import Brightstar from "./_components/brightstar";
+import Ubuntu from "./_components/ubuntu";
+import Other from "./_components/other";
 
 export default function Gallery() {
-  return <div>Gallery page</div>;
+  return (
+    <section className="max-width py-20 lg:py-44">
+      <h1 className="text-center">Gallery</h1>
+
+      <div className="space-y-12">
+        <Brightstar />
+        <Ubuntu />
+        <Other />
+      </div>
+    </section>
+  );
 }
