04c11e2a8a704223b7c4f4cfb74f4d23a7cdd110
sankthomas <thomassankara74@gmail.com>
2023-12-29 07:44:46 +0300

    refactor: our programs

--- app/our-programs/[programs]/page.js
+++ app/our-programs/[programs]/page.js
@@ -1,5 +1,30 @@
-import React from "react";
+"use client";
+
+import { programs } from "@/lib/data";
+import { useState } from "react";
 
 export default function OurPrograms({ params }) {
-  return <>Programs</>;
+  const [program, setProgram] = useState(programs);
+
+  const slug = params.programs;
+
+  console.log(slug);
+
+  // useEffect(() => {
+
+  // }, [params]);
+
+  return (
+    <>
+      <section className="max-width py-20 lg:py-44">
+        {/* {params.map((program) => (
+          <div key={program.title}>
+            <h1>{program.title}</h1>
+          </div>
+        ))} */}
+
+        <h1>Our Programs</h1>
+      </section>
+    </>
+  );
 }
