c70f1dd706f15bed669c1be7f97468eb19a95d0f
sankthomas <thomassankara74@gmail.com>
2023-12-30 15:35:56 +0300

    create: sanity blog

---
+++ blog/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'gjq57k61',
+    dataset: 'production'
+  }
+})
---
+++ blog/schemas/index.js
@@ -0,0 +1,9 @@
+import blockContent from './blockContent'
+import category from './category'
+import post from './post'
+import author from './author'
+import program from './program'
+import story from './story'
+import gallery from './gallery'
+
+export const schemaTypes = [post, author, category, blockContent, program, story, gallery]
