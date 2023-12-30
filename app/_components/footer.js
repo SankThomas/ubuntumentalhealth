8156643c8109d2b108b99ee75498f4148b672acf
sankthomas <thomassankara74@gmail.com>
2023-12-30 15:38:36 +0300

    refactor: add dark background color

--- app/_components/footer.js
+++ app/_components/footer.js
@@ -3,10 +3,12 @@ import SocialIcons from "./socialIcons";
 
 export default function Footer() {
   return (
-    <footer className="py-20 px-4 max-width border-t space-y-8 text-center">
-      <h3>Connect with us on social media</h3>
-      <div className="flex items-center justify-center">
-        <SocialIcons />
+    <footer className="bg-slate-900 space-y-8 text-center text-white">
+      <div className="max-width py-20">
+        <h3 className="text-white mb-4">Connect with us on social media</h3>
+        <div className="flex items-center justify-center">
+          <SocialIcons />
+        </div>
       </div>
     </footer>
   );
