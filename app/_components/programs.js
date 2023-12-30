12b79154070b3b8544599ffdcc5fafc1f2e5398e
sankthomas <thomassankara74@gmail.com>
2023-12-30 15:39:17 +0300

    refactor: update programs section

--- app/_components/programs.js
+++ app/_components/programs.js
@@ -1,37 +1,62 @@
-import React from "react";
-import { programs } from "@/lib/data";
-import Link from "next/link";
+import { createClient, groq } from "next-sanity";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { client } from "../../client";
 
-export default function Programs() {
+async function getPrograms() {
+  return createClient(client).fetch(
+    groq`*[_type == "program"] {
+      title,
+      slug,
+      body,
+      categories,
+      mainImage {
+        asset -> {
+          _id,
+          url
+        },
+        alt,
+      },
+    }`
+  );
+}
+
+getPrograms();
+
+export default async function Blog() {
+  const programs = await getPrograms();
+
   return (
     <>
-      <section className="max-width py-20 px-4">
-        <h2 className="text-slate-800 text-3xl lg:text-4xl font-bold mb-8">
-          Our programs
-        </h2>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {programs.map((program, index) => (
+      <section className="max-width py-20">
+        <h2 className="text-3xl lg:text-4xl text-center">Our Programs</h2>
+
+        <div className="grid grid-cols-1 gap-8 mt-16 md:grid-cols-2 lg:grid-cols-3">
+          {programs.map((program) => (
             <Link
-              href={program.href}
-              key={index}
-              className="border p-4 rounded-lg hover:bg-slate-100 transition space-y-4"
+              key={program.slug.current}
+              href={`/our-programs/${program.slug.current}`}
             >
-              <Image
-                src={program.image}
-                width={500}
-                height={300}
-                alt={program.title}
-                className="rounded-lg"
-              />
-              <h3 className="text-2xl font-semibold text-slate-700">
-                {program.title}
-              </h3>
-              <p>{`${program.desc.substring(0, 200)}...`}</p>
-              <Button variant="destructive" size="lg">
-                Continue reading
-              </Button>
+              <article className="border border-slate-600 rounded-lg hover:bg-slate-100 transition-all duration-200">
+                {program.mainImage ? (
+                  <Image
+                    src={program.mainImage.asset.url}
+                    width={800}
+                    height={900}
+                    alt="Blog"
+                    className="block mx-auto rounded-lg md:h-[300px] w-full object-cover"
+                  />
+                ) : null}
+
+                <div className="space-y-4 p-4">
+                  <h2 className="text-2xl mb-2">{program.title}</h2>
+
+                  <p className="text-slate-600 text-sm">{`${program.body[0].children[0].text.substring(
+                    0,
+                    100
+                  )}...`}</p>
+                </div>
+              </article>
             </Link>
           ))}
         </div>
