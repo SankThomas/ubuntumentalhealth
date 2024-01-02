ea146d786cc2e4bf7d7d31febe6c78b2402839b5
sankthomas <thomassankara74@gmail.com>
2024-01-02 21:50:32 +0300

    update gallery images

--- app/gallery/_components/brightstar.js
+++ app/gallery/_components/brightstar.js
@@ -2,39 +2,63 @@ import Photos from "./Photos";
 
 const brightstarPhotos = [
   {
-    path: "https://images.unsplash.com/photo-1504966981333-1ac8809be1ca?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    path: "/brightstar/brightstar-1.jpeg",
     description: "Some descriptive text about the photo",
   },
   {
-    path: "https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    path: "/brightstar/brightstar-2.jpeg",
     description: "Some descriptive text about the photo",
   },
   {
-    path: "https://images.unsplash.com/photo-1488866022504-f2584929ca5f?q=80&w=2062&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    path: "/brightstar/brightstar-3.jpeg",
     description: "Some descriptive text about the photo",
   },
   {
-    path: "https://images.unsplash.com/photo-1504966981333-1ac8809be1ca?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    path: "/brightstar/brightstar-4.jpeg",
     description: "Some descriptive text about the photo",
   },
   {
-    path: "https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    path: "/brightstar/brightstar-5.jpeg",
     description: "Some descriptive text about the photo",
   },
   {
-    path: "https://images.unsplash.com/photo-1488866022504-f2584929ca5f?q=80&w=2062&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    path: "/brightstar/brightstar-6.jpeg",
     description: "Some descriptive text about the photo",
   },
   {
-    path: "https://images.unsplash.com/photo-1504966981333-1ac8809be1ca?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    path: "/brightstar/brightstar-7.jpeg",
     description: "Some descriptive text about the photo",
   },
   {
-    path: "https://images.unsplash.com/photo-1444703686981-a3abbc4d4fe3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    path: "/brightstar/brightstar-8.jpeg",
     description: "Some descriptive text about the photo",
   },
   {
-    path: "https://images.unsplash.com/photo-1488866022504-f2584929ca5f?q=80&w=2062&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    path: "/brightstar/brightstar-9.jpeg",
+    description: "Some descriptive text about the photo",
+  },
+  {
+    path: "/brightstar/brightstar-10.jpeg",
+    description: "Some descriptive text about the photo",
+  },
+  {
+    path: "/brightstar/brightstar-11.jpeg",
+    description: "Some descriptive text about the photo",
+  },
+  {
+    path: "/brightstar/brightstar-12.jpeg",
+    description: "Some descriptive text about the photo",
+  },
+  {
+    path: "/brightstar/brightstar-13.jpeg",
+    description: "Some descriptive text about the photo",
+  },
+  {
+    path: "/brightstar/brightstar-14.jpeg",
+    description: "Some descriptive text about the photo",
+  },
+  {
+    path: "/brightstar/brightstar-15.jpeg",
     description: "Some descriptive text about the photo",
   },
 ];
@@ -43,7 +67,7 @@ export default function Brightstar() {
   return (
     <>
       <div>
-        <h2 className="text-3xl mb-4">Brightstar Initiative</h2>
+        <h2 className="text-3xl mb-4">The Bright Star Initiative</h2>
         <Photos photos={brightstarPhotos} title="Brightstar Initiative" />
       </div>
     </>
