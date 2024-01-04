04224c0e554070e4c687bba0e8af89ae5a9771e6
sankthomas <thomassankara74@gmail.com>
2024-01-04 12:33:52 +0300

    update: change welcome banner text

--- app/_components/homepage/banner.js
+++ app/_components/homepage/banner.js
@@ -1,24 +1,35 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function Banner() {
   return (
     <section className="showcase">
-      <div className="relative z-50 bg-black/25 flex items-center justify-center text-center h-screen">
+      <div className="relative z-50 bg-black/35 flex items-center justify-center text-center h-screen">
         <article className="space-y-8">
           <h1 className="text-white">Ubuntu Mental Health</h1>
           <p className="lg:w-1/2 mx-auto text-white/75 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-            sit ducimus quaerat numquam quibusdam ipsam qui minus quod
-            voluptates perspiciatis?
+            Welcome to Ubuntu Mental Health, where we believe in the
+            transformative power of unity and interconnectedness.
           </p>
 
-          <Button variant="outline">Meet the team</Button>
+          <ul className="flex items-center justify-center gap-4 flex-wrap">
+            <li>
+              <Button asChild variant="secondary">
+                <Link href="/about-us">About Us</Link>
+              </Button>
+            </li>
+            <li>
+              <Button asChild variant="custom">
+                <Link href="/team">Meet the team</Link>
+              </Button>
+            </li>
+          </ul>
         </article>
       </div>
 
-      <div className="hidden lg:block absolute -bottom-20 lg:bottom-0 right-0 left-0 w-full">
+      {/* <div className="hidden lg:block absolute -bottom-20 lg:bottom-0 right-0 left-0 w-full">
         <Image
           src="/banner.png"
           width={2560}
@@ -26,7 +37,7 @@ export default function Banner() {
           alt="Banner"
           className="w-full"
         />
-      </div>
+      </div> */}
     </section>
   );
 }
