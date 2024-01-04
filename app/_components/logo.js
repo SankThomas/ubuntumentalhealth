6ca0c62c60f8db3acfcc7c41def307b5976d9471
sankthomas <thomassankara74@gmail.com>
2024-01-04 12:33:20 +0300

    create: logo component

---
+++ app/_components/logo.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function Logo() {
+  return (
+    <Link href="/">
+      <Image
+        src="/logo.png"
+        width={60}
+        height={60}
+        alt="Ubuntu Mental Health Logo"
+      />
+    </Link>
+  );
+}
