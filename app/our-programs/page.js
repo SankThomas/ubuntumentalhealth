9312a4c46b618c58fea07734281d22d010171507
sankthomas <thomassankara74@gmail.com>
2024-01-04 10:25:20 +0300

    refactor: update page

--- app/our-programs/page.js
+++ app/our-programs/page.js
@@ -2,6 +2,8 @@ import { createClient, groq } from "next-sanity";
 import Image from "next/image";
 import Link from "next/link";
 import { client } from "../../client";
+import { Card, CardDescription, CardHeader } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 export const metadata = {
   title: "Our Programs",
@@ -35,32 +37,49 @@ export default async function Blog() {
       <section className="max-width py-20 lg:py-44">
         <h1 className="text-center">Our Programs</h1>
 
-        <div className="grid grid-cols-1 gap-8 mt-16 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 gap-8 mt-16">
           {programs.map((program) => (
             <Link
               key={program._id}
               href={`/our-programs/${program.slug.current}`}
             >
-              <article className="border border-slate-600 rounded-lg hover:bg-slate-100 transition-all duration-200">
-                {program.mainImage ? (
-                  <Image
-                    src={program.mainImage.asset.url}
-                    width={800}
-                    height={900}
-                    alt="Blog"
-                    className="block mx-auto rounded-lg md:h-[300px] w-full object-cover"
-                  />
-                ) : null}
+              <Card className="md:flex items-center justify-center hover:bg-slate-100 transition">
+                <CardHeader className="md:flex-1">
+                  {program.mainImage ? (
+                    <Image
+                      src={program.mainImage.asset.url}
+                      width={800}
+                      height={900}
+                      alt="Blog"
+                      className="block mx-auto rounded-lg md:h-[300px] w-full object-cover"
+                    />
+                  ) : null}
+                </CardHeader>
 
-                <div className="space-y-4 p-4">
-                  <h2 className="text-2xl mb-2">{program.title}</h2>
+                <CardDescription className="px-6 pb-6 md:flex-1">
+                  <article>
+                    <div className="space-y-4">
+                      <h2 className="text-2xl mb-2">{program.title}</h2>
 
-                  <p className="text-slate-600 text-sm">{`${program.body[0].children[0].text.substring(
-                    0,
-                    100
-                  )}...`}</p>
-                </div>
-              </article>
+                      <p className="text-slate-600 text-sm md:hidden">{`${program.body[0].children[0].text.substring(
+                        0,
+                        100
+                      )}...`}</p>
+
+                      <p className="text-slate-600 text-sm hidden md:block">{`${program.body[0].children[0].text.substring(
+                        0,
+                        300
+                      )}...`}</p>
+
+                      <Button asChild variant="ghost">
+                        <Link href={`/our-programs/${program.slug.current}`}>
+                          Read More &rarr;
+                        </Link>
+                      </Button>
+                    </div>
+                  </article>
+                </CardDescription>
+              </Card>
             </Link>
           ))}
         </div>
